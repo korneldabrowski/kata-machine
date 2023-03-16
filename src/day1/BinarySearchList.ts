04b26ed11f7357321320efa7617b9d3cfae5ded7
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-16 21:18:18 +0100

    Binary Search implemented

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const m = Math.floor(low + (high - low) / 2);
+        const value = haystack[m];
+
+        if (value === needle) {
+            return true;
+        } else if (needle < value) {
+            high = m;
+        } else {
+            low = m + 1;
+        }
+    } while (low < high);
+    return false;
 }
