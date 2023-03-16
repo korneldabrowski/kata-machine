67a5ccc75838e625dfea3def5fae0918b4b27e3d
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-17 00:50:35 +0100

    Two crystal balls implementation

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpValue = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpValue;
+
+    for (; i < breaks.length; i += jumpValue) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpValue;
+
+    for (let j = 0; j < jumpValue && j < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
