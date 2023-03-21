82d836417af8a02748a48794f3674e4a18f0c277
Kornel Dabrowski <korneldabrowski@gmail.com>
2023-03-21 11:52:06 +0100

    Implemented Stack DS

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,40 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        const head = this.head;
+        if (this.length === 0) {
+            this.head = undefined;
+            return head?.value;
+        }
 
-}
+        this.head = head?.prev;
+        return head?.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
